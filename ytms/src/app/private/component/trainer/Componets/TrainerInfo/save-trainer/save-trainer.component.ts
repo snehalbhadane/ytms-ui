f528130d85893164fe2e3607d7ce8c84c6531767
Pushpendra Kumar Pal <pushpendra.pal@Yash.com>
2023-12-04 21:54:10 +0530

    Trainer validation

--- ytms/src/app/private/component/trainer/Componets/TrainerInfo/save-trainer/save-trainer.component.ts
+++ ytms/src/app/private/component/trainer/Componets/TrainerInfo/save-trainer/save-trainer.component.ts
@@ -3,7 +3,7 @@ import { Trainer } from '../../../Entity/trainer';
 import { TrainerprofileService } from '../../../Services/trainerprofile.service';
 import { Router } from '@angular/router';
 import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
-import { FormGroup,FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -12,61 +12,61 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./save-trainer.component.css']
 })
 export class SaveTrainerComponent implements OnInit {
-  saveTrainerComponent:Trainer=new Trainer();
-  
-  addTrainer= new FormGroup({
+  saveTrainerComponent: Trainer = new Trainer();
 
-    
-    currentLocation :new FormControl(''),
-    baseLocation:new FormControl(''),
-    irm:new FormControl(''),
-    type:new FormControl(''),
-    totalExperience:new FormControl(''),
-    user:new FormGroup({
-    userId:new FormControl('')
+  addTrainer = new FormGroup({
+
+
+    currentLocation: new FormControl('', Validators.required),
+    baseLocation: new FormControl('', Validators.required),
+    irm: new FormControl('', Validators.required),
+    type: new FormControl('', Validators.required),
+    totalExperience: new FormControl('', Validators.required),
+    user: new FormGroup({
+      userId: new FormControl('', Validators.required)
 
     })
-});
-// profileForm = new FormGroup({
-//   firstName: new FormControl(''),
-//   lastName: new FormControl(''),
-//   address: new FormGroup({
-//     street: new FormControl(''),
-//     city: new FormControl(''),
-//     state: new FormControl(''),
-//     zip: new FormControl('')
-//   })
-// });
-  constructor(private trainerprofileService :TrainerprofileService,private _router: Router,private toastrService: ToastrService) { }
+  });
+  // profileForm = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl(''),
+  //   address: new FormGroup({
+  //     street: new FormControl(''),
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     zip: new FormControl('')
+  //   })
+  // });
+  constructor(private trainerprofileService: TrainerprofileService, private _router: Router, private toastrService: ToastrService) { }
 
   ngOnInit(): void {
 
-    
+
   }
-// saveTrainer(){
-  
-//   this.trainerprofileService.addTrainer(this.saveTrainerComponent).subscribe((data:any)=>{
-//  console.log(data);
-//   },
-//   (error) => {
-//     console.log(error);
-//   }
-//   )
-  
-// }
+  // saveTrainer(){
 
-onSubmit(){
-  console.log(this.addTrainer.value);
-  this.trainerprofileService.addTrainer(this.addTrainer.value).subscribe((resutlt:any)=>{
-    this.toastrService.success('Trainer saved!', 'Success');
-    console.log(resutlt);
+  //   this.trainerprofileService.addTrainer(this.saveTrainerComponent).subscribe((data:any)=>{
+  //  console.log(data);
+  //   },
+  //   (error) => {
+  //     console.log(error);
+  //   }
+  //   )
 
- },(error) => {
+  // }
 
-       console.log(error);
-   }
- )
-  
- 
-}
+  onSubmit() {
+    console.log(this.addTrainer.value);
+    this.trainerprofileService.addTrainer(this.addTrainer.value).subscribe((resutlt: any) => {
+      this.toastrService.success('Trainer saved!', 'Success');
+      console.log(resutlt);
+
+    }, (error) => {
+
+      console.log(error);
+    }
+    )
+
+
+  }
 }
